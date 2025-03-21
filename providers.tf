d991c74649aae70d20ab2e84500161355e0df49b
hadoopblr <hadoopblr@gmail.com>
2025-03-21 12:48:21 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,5 +2,8 @@ provider "azurerm" {
     features {}
     
 subscription_id = "38bd44d5-d91c-4f34-8f1e-c946d6697ae4"
+tenant_id = "d0f66b2a-6d50-4e11-b7a6-e0fe38d06609"  
+client_id = "cb462aac-596c-4dd2-aa03-2bb6f4762e3e"
+client_secret = "Bm08Q~3UZJEqeO2hDKjIomDomIEJ4Z2xGheljb86"
 }
 
