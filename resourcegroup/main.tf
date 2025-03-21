cea0e8460dc0b39799a6f79963c1380bff998cdb
hadoopblr <hadoopblr@gmail.com>
2025-03-21 12:42:22 +0530

    Update main.tf

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -1,7 +1,4 @@
 resource "azurerm_resource_group" "example" {
-    name = "demo-rg-${var.env}"
-    location = var.location 
-    tags = {
-        environment = var.env
-    }
-}
+    name = "demo-rg-dev"
+    location = "east us" 
+   }
