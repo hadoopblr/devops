fc686224bd72be2b906dbfb380d7008a7b7e8722
hadoopblr <hadoopblr@gmail.com>
2025-03-21 12:30:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "resourcegroup" {
-  source  = "../resourcegroup"
+  source  = "resourcegroup"
   env     = var.env
   location = var.location
 }
 
 
 module "virtualmachine" {
-  source  = "../virtualmachine"
+  source  = "virtualmachine"
   vnet_name     = var.vnet_name
   location = var.location
   env =var.env
